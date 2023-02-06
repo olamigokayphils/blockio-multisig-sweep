f96542bbed61115df7cb40fa561876ac1501e99d
Olamigoke Philip <olamigokayphils@gmail.com>
2023-02-06 17:13:44 +0100

    initial cleanup & repo update

--- src/constants.js
+++ src/constants.js
@@ -1,40 +1,45 @@
 module.exports = {
-  P2WSH_P2SH: 'P2WSH-P2SH',
-  P2SH: 'P2SH',
-  P2WSH: 'WITNESS_V0',
-  COIN: '100000000',
+  P2WSH_P2SH: "P2WSH-P2SH",
+  P2SH: "P2SH",
+  P2WSH: "WITNESS_V0",
+  COIN: "100000000",
   N: 100,
   MAX_TX_INPUTS: 500,
-  BLOCKCHAIN_PROVIDER_DEFAULT: 'sochain',
-  BLOCKCHAIN_PROVIDER_URL_DEFAULT: 'https://sochain.com/api/v2/',
+  BLOCKCHAIN_PROVIDER_DEFAULT: "blockchaincom",
+  BLOCKCHAIN_PROVIDER_URL_DEFAULT: "https://blockchain.info",
   // the order of provider names matters in PROVIDERS and PROVIDER_URLS
   // the order needs to be the same
   PROVIDERS: {
-    SOCHAIN: 'sochain',
-    MEMPOOLSPACE: 'mempoolspace',
-    BLOCKCHAINCOM: 'blockchaincom'
+    MEMPOOLSPACE: "mempoolspace",
+    BLOCKCHAINCOM: "blockchaincom",
+    BLOCKCYPHER: "blockcypher",
+    SOCHAIN: "sochain",
   },
   PROVIDER_URLS: {
-    SOCHAIN: {
-      URL: 'https://sochain.com/api/v2',
-	SUPPORT: ['BTC', 'LTC', 'DOGE', 'BTCTEST', 'DOGETEST', 'LTCTEST']
-    },
     MEMPOOLSPACE: {
-      URL: 'https://mempool.space',
-      SUPPORT: ['BTC', 'BTCTEST']
+      URL: "https://mempool.space",
+      SUPPORT: ["BTC", "BTCTEST"],
     },
     BLOCKCHAINCOM: {
-      URL: 'https://blockchain.info',
-      SUPPORT: ['BTC']
-    }
+      URL: "https://blockchain.info",
+      SUPPORT: ["BTC"],
+    },
+    BLOCKCYPHER: {
+      URL: "https://api.blockcypher.com/v1/btc/main",
+      SUPPORT: ["BTC"],
+    },
+    SOCHAIN: {
+      URL: "https://sochain.com/api/v2",
+      SUPPORT: ["BTC", "LTC", "DOGE", "BTCTEST", "DOGETEST", "LTCTEST"],
+    },
   },
   NETWORKS: {
-    BTC: 'BTC',
-    BTCTEST: 'BTCTEST',
-    LTC: 'LTC',
-    LTCTEST: 'LTCTEST',
-    DOGE: 'DOGE',
-    DOGETEST: 'DOGETEST'
+    BTC: "BTC",
+    BTCTEST: "BTCTEST",
+    LTC: "LTC",
+    LTCTEST: "LTCTEST",
+    DOGE: "DOGE",
+    DOGETEST: "DOGETEST",
   },
   FEE_RATE: {
     BTC: 20,
@@ -42,7 +47,7 @@ module.exports = {
     DOGE: 2000,
     BTCTEST: 20,
     LTCTEST: 20,
-    DOGETEST: 2000
+    DOGETEST: 2000,
   },
   DUST: {
     BTC: 546,
@@ -50,15 +55,15 @@ module.exports = {
     DOGE: 1000000, // https://github.com/dogecoin/dogecoin/blob/v1.14.5/doc/fee-recommendation.md
     BTCTEST: 546,
     LTCTEST: 1000,
-    DOGETEST: 1000000 // https://github.com/dogecoin/dogecoin/blob/v1.14.5/doc/fee-recommendation.md
+    DOGETEST: 1000000, // https://github.com/dogecoin/dogecoin/blob/v1.14.5/doc/fee-recommendation.md
   },
   NETWORK_FEE_MAX: {
-    BTC: (250 * 100000), // 0.25 BTC
-    BTCTEST: (250 * 100000), // 0.25 BTCTEST
-    LTC: (50 * 100000), // 0.05 LTC
-    LTCTEST: (50 * 100000), // 0.05 LTCTEST
-    DOGE: (2 * 100000000), // 2.00 DOGE
-    DOGETEST: (2 * 100000000) // 2.00 DOGETEST
+    BTC: 250 * 100000, // 0.25 BTC
+    BTCTEST: 250 * 100000, // 0.25 BTCTEST
+    LTC: 50 * 100000, // 0.05 LTC
+    LTCTEST: 50 * 100000, // 0.05 LTCTEST
+    DOGE: 2 * 100000000, // 2.00 DOGE
+    DOGETEST: 2 * 100000000, // 2.00 DOGETEST
   },
-  TX_BROADCAST_APPROVAL_TEXT: 'I have verified this transaction, and I want to broadcast it now'
-}
+  TX_BROADCAST_APPROVAL_TEXT: "I have verified this transaction, and I want to broadcast it now",
+};
--- src/services/ProviderService.js
+++ src/services/ProviderService.js
@@ -1,112 +1,119 @@
-const constants = require('../constants')
-const fetch = require('node-fetch')
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const constants = require("../constants");
+const fetch = require("node-fetch");
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const ProviderService = function (provider, network) {
-  const providerIndex = Object.values(constants.PROVIDERS).indexOf(provider)
+  const providerIndex = Object.values(constants.PROVIDERS).indexOf(provider);
   if (providerIndex < 0) {
-    throw new Error('Blockchain provider not supported')
+    throw new Error("Blockchain provider not supported");
   }
-  const providerKey = Object.keys(constants.PROVIDER_URLS)[providerIndex]
+  const providerKey = Object.keys(constants.PROVIDER_URLS)[providerIndex];
   if (constants.PROVIDER_URLS[providerKey].SUPPORT.indexOf(network) < 0) {
-    throw new Error('Network not supported by provider')
+    throw new Error("Network not supported by provider");
   }
-  this.network = network
-  this.provider = provider
-}
+  this.network = network;
+  this.provider = provider;
+};
 
 ProviderService.prototype.getTxHex = async function (txId) {
   try {
     switch (this.provider) {
       case constants.PROVIDERS.SOCHAIN: {
-        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, 'get_tx', this.network, txId].join('/')
-        const res = await fetchUrl(apiUrl)
-        const json = await res.json()
-        if (json.status === 'fail') {
-          throw new Error(JSON.stringify(json.data))
+        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, "get_tx", this.network, txId].join("/");
+        const res = await fetchUrl(apiUrl);
+        const json = await res.json();
+        if (json.status === "fail") {
+          throw new Error(JSON.stringify(json.data));
         }
-        return json.data.tx_hex
+        return json.data.tx_hex;
       }
       case constants.PROVIDERS.MEMPOOLSPACE: {
-        const networkType = this.network === constants.NETWORKS.BTC ? 'api' : 'testnet/api'
-        const apiUrl = [constants.PROVIDER_URLS.MEMPOOLSPACE.URL, networkType, 'tx', txId, 'hex'].join('/')
-        const res = await fetchUrl(apiUrl)
-        const hex = await res.text()
+        const networkType = this.network === constants.NETWORKS.BTC ? "api" : "testnet/api";
+        const apiUrl = [constants.PROVIDER_URLS.MEMPOOLSPACE.URL, networkType, "tx", txId, "hex"].join("/");
+        const res = await fetchUrl(apiUrl);
+        const hex = await res.text();
         if (res.status !== 200) {
-          throw new Error(hex)
+          throw new Error(hex);
         }
-        return hex
+        return hex;
       }
       case constants.PROVIDERS.BLOCKCHAINCOM: {
-        const apiUrl = [constants.PROVIDER_URLS.BLOCKCHAINCOM.URL, 'rawtx', txId, '?format=hex'].join('/')
-        const res = await fetchUrl(apiUrl)
-        const hex = await res.text()
+        const apiUrl = [constants.PROVIDER_URLS.BLOCKCHAINCOM.URL, "rawtx", txId, "?format=hex"].join("/");
+        const res = await fetchUrl(apiUrl);
+        const hex = await res.text();
         if (res.status !== 200) {
-          throw new Error(hex)
+          throw new Error(hex);
         }
-        return hex
+        return hex;
       }
+
       default: {
-        throw new Error('Could not get hex with provider: ' + this.provider)
+        throw new Error("Could not get hex with provider: " + this.provider);
       }
     }
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-}
+};
 
 ProviderService.prototype.getUtxo = async function (addr) {
   try {
     switch (this.provider) {
       case constants.PROVIDERS.SOCHAIN: {
-        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, 'get_tx_unspent', this.network, addr].join('/')
-        const res = await fetchUrl(apiUrl)
-        const json = await res.json()
-        if (json.status === 'fail') {
-          throw new Error(JSON.stringify(json.data))
+        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, "get_tx_unspent", this.network, addr].join("/");
+        const res = await fetchUrl(apiUrl);
+        const json = await res.json();
+        if (json.status === "fail") {
+          throw new Error(JSON.stringify(json.data));
         }
-        return json.data.txs
+        return json.data.txs;
       }
       case constants.PROVIDERS.BLOCKCHAINCOM: {
-        const apiUrl = [constants.PROVIDER_URLS.BLOCKCHAINCOM.URL, 'unspent?active=' + addr].join('/')
-        const res = await fetchUrl(apiUrl)
-        const json = await res.json()
+        const apiUrl = [constants.PROVIDER_URLS.BLOCKCHAINCOM.URL, "unspent?active=" + addr].join("/");
+        const res = await fetchUrl(apiUrl);
+        const json = await res.json();
         if (json.error) {
-          throw new Error(json.message)
+          throw new Error(json.message);
         }
-        return json.unspent_outputs
+        return json.unspent_outputs;
       }
       default: {
-        throw new Error('Could not get utxo with provider: ' + this.provider)
+        throw new Error("Could not get utxo with provider: " + this.provider);
       }
     }
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-}
+};
 
 ProviderService.prototype.sendTx = async function (txHex) {
   try {
     switch (this.provider) {
       case constants.PROVIDERS.SOCHAIN: {
-        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, 'send_tx', this.network].join('/')
-        await broadcastTx(apiUrl, txHex)
-        return
+        const apiUrl = [constants.PROVIDER_URLS.SOCHAIN.URL, "send_tx", this.network].join("/");
+        await broadcastTx(apiUrl, txHex);
+        return;
+      }
+
+      case constants.PROVIDERS.BLOCKCHAINCOM: {
+        const apiUrl = [constants.PROVIDER_URLS.BLOCKCYPHER, "txs", "push"].join("/");
+        await broadcastTx(apiUrl, txHex);
+        return;
       }
       default: {
-        throw new Error('Could not send tx with provider: ' + this.provider)
+        throw new Error("Could not send tx with provider: " + this.provider);
       }
     }
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-}
+};
 
-module.exports = ProviderService
+module.exports = ProviderService;
 
-async function fetchUrl (url) {
+async function fetchUrl(url) {
   try {
-    let response = await fetch(url)
+    let response = await fetch(url);
     if (response.ok) {
       return response;
     } else {
@@ -115,26 +122,36 @@ async function fetchUrl (url) {
       return await fetchUrl(url);
     }
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
 }
 
-async function broadcastTx (apiUrl, txHex) {
+async function broadcastTx(apiUrl, txHex) {
   try {
-    let res = await fetch(apiUrl, {
-      method: 'POST',
-      body: JSON.stringify({ tx_hex: txHex }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    res = await res.json()
-    if (res.status === 'success') {
-      console.log('Sweep Success!')
-      console.log('Tx_id:', res.data.txid)
+    let res;
+    if (apiUrl == [constants.PROVIDER_URLS.BLOCKCYPHER, "txs", "push"].join("/")) {
+      res = await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify({ tx: txHex }),
+        headers: { "Content-Type": "application/json" },
+      });
+      res = await res.json();
     } else {
-      console.log('Sweep Failed:')
-      throw new Error(JSON.stringify(res.data))
+      res = await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify({ tx_hex: txHex }),
+        headers: { "Content-Type": "application/json" },
+      });
+      res = await res.json();
+    }
+    if (res.status === "success") {
+      console.log("Sweep Success!");
+      console.log("Tx_id:", res.data.txid);
+    } else {
+      console.log("Sweep Failed:");
+      throw new Error(JSON.stringify(res.data));
     }
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
 }
